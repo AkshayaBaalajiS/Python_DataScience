5f339672200874e8bfba414224fd496ff9b17a05
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2026-03-04 10:10:30 +0530

    variable in python udnerstanding and variable are not he object in python

---
+++ Variables/variables.py
@@ -0,0 +1,10 @@
+iVar = 12
+string = "Akshaya Baalaji S"
+fvar= 12.212121
+print(iVar)
+print(string)
+print(fvar)
+
+# if no space needed 
+print(iVar , end= " ")
+print("This print will be on same line ")
